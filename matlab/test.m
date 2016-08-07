7e36b779467c5ac6f009b6f3e71ecb2dba32a0f6
Richard Peng <Richard Peng>
2016-08-07 16:03:29 -0400

    some fixed matlab logs

--- matlab/test.m
+++ matlab/test.m
@@ -2,7 +2,7 @@ clear
 
 digits(1024);
 
-folder = '../graphs/pathDisjoint_30_exp20/';
+folder = '../graphs/pathDisjoint_1000_exp20/';
 f_log = fopen('log.txt', 'w');
 
 fprintf(f_log, '=========CG using MATLAB variable precision=====\n');
@@ -14,7 +14,6 @@ fprintf('=========CG using MATLAB variable precision=====\n');
 fprintf('====NUMBER OF DIGITS = %d\n', digits);
 fprintf('====DATA = %s\n', folder);
 fprintf('====error given as ||Ax - b||_2 / ||b||_2 and ||x - xbar||_A / ||xbar||_A\n', folder);
-fclose(f_log);
 
 f_matrix = strcat(folder, 'graph.mtx');
 f_tree = strcat(folder, 'tree.mtx');
@@ -31,8 +30,13 @@ onesN = vpa(ones(n, 1) / n);
 
 b2 = norm(b);
 xbarA = sqrt(xbar' * LG * xbar);
+
+fprintf(f_log, '||b||_2 = %f\n', b2);
+fprintf(f_log, '||xbar||_A = %f\n', xbarA);
 fprintf('||b||_2 = %f\n', b2);
 fprintf('||xbar||_A = %f\n', xbarA);
+
+fclose(f_log);
 
 %[sum(xbar), sum(b)]
 
@@ -53,8 +57,8 @@ for iter = 1:1000
     r = r - alpha * LG * p; %b - LG * x;
     
     f_log = fopen('log.txt', 'a');
-    fprintf(f_log,'i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, (x - xbar)'*LG*(x - xbar) / xbarA);
-    fprintf('i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, (x - xbar)'*LG*(x - xbar) / xbarA);
+    fprintf(f_log,'i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, sqrt((x - xbar)'*LG*(x - xbar)) / xbarA);
+    fprintf('i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, sqrt((x - xbar)'*LG*(x - xbar)) / xbarA);
     fclose(f_log);
     
     z = [LT(1:n-1,1:n-1) \ r(1:n-1); 0];
--- matlab/testDefault.m
+++ matlab/testDefault.m
@@ -2,15 +2,17 @@ clear
 
 f_log = fopen('log.txt', 'w');
 
-folder = '../graphs/se_2/';
+folder = '../graphs/pathDisjoint_1000_exp20/';
 
-fprintf(f_log, '=========CG using MATLAB default precision=====\n');
+fprintf(f_log, '=========CG using MATLAB variable precision=====\n');
+fprintf(f_log, '====NUMBER OF DIGITS = %d\n', digits);
 fprintf(f_log, '====DATA = %s\n', folder);
-fprintf(f_log, '====error given as ||Ax - b||_2^2 and ||x - xbar||_A^2\n', folder);
+fprintf(f_log, '====error given as ||Ax - b||_2 / ||b||_2 and ||x - xbar||_A / ||xbar||_A\n', folder);
 
-fprintf('=========CG using MATLAB default precision=====\n');
+fprintf('=========CG using MATLAB variable precision=====\n');
+fprintf('====NUMBER OF DIGITS = %d\n', digits);
 fprintf('====DATA = %s\n', folder);
-fprintf('====error given as ||Ax - b||_2^2 and ||x - xbar||_A^2\n', folder);
+fprintf('====error given as ||Ax - b||_2 / ||b||_2 and ||x - xbar||_A / ||xbar||_A\n', folder);
 
 f_matrix = strcat(folder, 'graph.mtx');
 f_tree = strcat(folder, 'tree.mtx');
@@ -21,6 +23,15 @@ LG = getLaplacian(f_matrix);
 LT = getLaplacian(f_tree);
 b = getVector(f_b);
 xbar = getVector(f_x);
+
+b2 = norm(b);
+xbarA = sqrt(xbar' * LG * xbar);
+
+fprintf(f_log, '||b||_2 = %f\n', b2);
+fprintf(f_log, '||xbar||_A = %f\n', xbarA);
+fprintf('||b||_2 = %f\n', b2);
+fprintf('||xbar||_A = %f\n', xbarA);
+
 
 %[sum(xbar), sum(b), norm(LG * xbar - b)]
 
@@ -38,7 +49,7 @@ p = z;
 
 %p' * LG * p
 
-for iter = 1:10000
+for iter = 1:1000
     alpha = (r' * z) / (p' * LG * p);
     x = x + alpha * p;
    
@@ -48,9 +59,8 @@ for iter = 1:10000
     
     r = r - alpha * LG * p; %b - LG * x;
 
-    
-    fprintf(f_log, 'i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r)^2, (x - xbar)'*LG*(x - xbar));
-    fprintf('i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r)^2, (x - xbar)'*LG*(x - xbar));
+    fprintf(f_log,'i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, sqrt((x - xbar)'*LG*(x - xbar)) / xbarA);
+    fprintf('i=%3d, err2=%0.6g, errA = %0.6g\n', iter, norm(r) / b2, sqrt((x - xbar)'*LG*(x - xbar)) / xbarA);
     
     z = [LT(1:n-1,1:n-1) \ r(1:n-1); 0];
     z = z - sum(z) * onesN;
