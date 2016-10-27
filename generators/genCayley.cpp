f9bc564544732246c6ae0fe96eddd078ede7442c
Richard Peng <Richard Peng>
2016-10-26 20:13:41 -0400

    ran cpplint

--- generators/genCayley.cpp
+++ generators/genCayley.cpp
@@ -18,8 +18,9 @@ int n, k;
 
 
 int main(int argt, char **args) {
-  if(argt <= 1 || (argt - 2) % 2 == 1) {
-    fprintf(stderr, "INCORRECT # OF ARGS, should be n followed by 2k info about generators\n");
+  if (argt <= 1 || (argt - 2) % 2 == 1) {
+    fprintf(stderr, "INCORRECT # OF ARGS,");
+    fprintf(stderr, "should be n followed by 2k info about generators\n");
     exit(0);
   }
 
@@ -27,9 +28,10 @@ int main(int argt, char **args) {
   sscanf(args[1], "%d", &n);
   printf("%d %d\n", n, k * n);
 
-  fprintf(stderr, "MAKING CAYLEY GRAPH ON %d VERTICES WITH %d GENERATORS\n", n, k);
+  fprintf(stderr, "MAKING CAYLEY GRAPH ON %d VERTICES");
+  fprintf(stderr, "WITH %d GENERATORS\n", n, k);
 
-  for(int i = 0; i < k; ++i) {
+  for (int i = 0; i < k; ++i) {
     int skip, weight;
 
     sscanf(args[2 + i * 2], "%d", &skip);
@@ -38,7 +40,7 @@ int main(int argt, char **args) {
     fprintf(stderr, "EDGE TYPE %d: SKIP = %d, WEIGHT = %d\n", i, skip, weight);
 
 
-    for(int j = 0; j < n; ++j) {
+    for (int j = 0; j < n; ++j) {
       printf("%d %d %d\n", j + 1, (j + skip) % n + 1, weight);
     }
   }
