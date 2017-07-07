54d7283ad4174c6343371f8a3f33913fac646326
Kevin Deweese <kevin.g.deweese@gmail.com>
2017-07-07 11:25:21 -0700

    changed to Laplacian mmread in cg_eig_bad script

--- cg_eig_bad/cg_solver.cpp
+++ cg_eig_bad/cg_solver.cpp
@@ -9,23 +9,24 @@
 int main(int argc, char *argv[]) {
     string dir = argv[1];
     if (dir[dir.length()-1] != '/') dir+="/";
-    Mat A = IO::readMMA(dir+"mat.mtx");
+    Mat L = IO::readMML(dir+"mat.mtx");
 //    Mat tree = IO::readMML(dir+"tree.mtx");
     
     // Vec x(A.n);
     // rep(i,0,A.n-1) x[i]=(double(rand())/double(RAND_MAX)-0.5)*10;
     // Vec b=A*x;
     Vec x = IO::readMMVec(dir+"x.vec");
-    Vec b = A*x;
-    AbstractSolver S = PCG(A);
-
+    Vec b = L*x;
+    
+    AbstractSolver S = PCG(L);
+    
     int flag; FLOAT relres; int iter; vector<FLOAT> resvec;
     clock_t t_start = clock();
-    tie(x, flag, relres, iter, resvec) = S.solve(b, 1e-6);
+    tie(x, flag, relres, iter, resvec) = S.solve(b, 1e-6, 4);
     clock_t t_end = clock();
     double tcost = static_cast<double>(t_end - t_start) /
         static_cast<double>(CLOCKS_PER_SEC);
     printf("n = %d iter = %d time = %.3lfs relres = %.16lf\n",
-        A.n, iter, tcost, printFloat((A*x-b).norm()/b.norm()));
+        L.n, iter, tcost, printFloat((L*x-b).norm()/b.norm()));
     return 0;
 }
